df096b95f796e432bc57e9673f15959c03b19a97
lucasbassetto <lucasfbassetto@gmail.com>
2025-11-14 16:02:47 -0300

    feat: add another exemple

--- src/main/java/org/example/ForWhile/SumRevenue.java
+++ src/main/java/org/example/ForWhile/SumRevenue.java
@@ -1,8 +1,26 @@
 package org.example.ForWhile;
 
+import java.util.Scanner;
+
 public class SumRevenue {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] valores = new int[2];
+        int soma = 0;
+
+        for (int i = 0; i < valores.length; i++) {
+            System.out.print("Digite o valor " + (i + 1) + ": ");
+            valores[i] = scanner.nextInt();
+        }
+
+        for (int value : valores) {
+            soma += value;
+        }
+        System.out.println("Total Revenue: " + soma);
+
+        System.out.println("--------------------------------");
 
         int[] values = {10, 20, 30, 40, 50};
         int sum = 0;
