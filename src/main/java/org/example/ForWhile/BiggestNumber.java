00e2e99e813e0e33bf3841174824d0ab0d1d6d9d
lucasbassetto <lucasfbassetto@gmail.com>
2025-11-26 07:51:13 -0300

    feat: add BiggestNumber exercise

---
+++ src/main/java/org/example/ForWhile/BiggestNumber.java
@@ -0,0 +1,29 @@
+package org.example.ForWhile;
+
+import java.util.Scanner;
+
+public class BiggestNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Type a series of integers separated by spaces: ");
+        String[] n = scanner.nextLine().split(" ");
+        int biggest = Integer.MIN_VALUE;
+
+        for(String numberStr : n){
+            try {
+                int number = Integer.parseInt(numberStr);
+                if(number > biggest){
+                    biggest = number;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid: " + numberStr + " isn't a integer number.");
+            }
+        }
+
+
+
+    }
+}
