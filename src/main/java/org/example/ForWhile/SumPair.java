aeb1f6b00aaf03541ccc1a863d24c782cafa5e66
lucasbassetto <lucasfbassetto@gmail.com>
2025-11-26 07:39:57 -0300

    feat: add SumPair exercice

---
+++ src/main/java/org/example/ForWhile/SumPair.java
@@ -0,0 +1,18 @@
+package org.example.ForWhile;
+
+public class SumPair {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for(int i = 1; i<= 100; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println("Sum of even numbers from 1 to 100: " + sum);
+
+    }
+}
