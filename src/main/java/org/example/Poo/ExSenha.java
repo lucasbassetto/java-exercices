5b80a2df916dc3940e9d2e6638f65b01fcb8f364
lucasbassetto <lucasfbassetto@gmail.com>
2026-03-04 14:53:33 -0300

    feat: adicionando ExSenha

---
+++ src/main/java/org/example/Poo/ExSenha.java
@@ -0,0 +1,11 @@
+package org.example.Poo;
+
+public class ExSenha {
+
+    public static void main(String[] args) {
+
+        Usuario user = new Usuario("123");
+
+        user.setSenha("123", "12");
+    }
+}
---
+++ src/main/java/org/example/Poo/Usuario.java
@@ -0,0 +1,19 @@
+package org.example.Poo;
+
+public class Usuario {
+
+    private String senha;
+
+    public Usuario(String senha) {
+        this.senha = senha;
+    }
+
+    public void setSenha(String senhaAtual, String novaSenha) {
+        if (senhaAtual.equals(this.senha)) {
+            this.senha = novaSenha;
+            System.out.println("Senha alterada com sucesso!");
+        } else {
+            System.out.println("Senha atual incorreta. A senha não foi alterada.");
+        }
+    }
+}
