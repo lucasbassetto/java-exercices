e80ac2ff26840ede9d4b0420204b9ca3266642ec
lucasbassetto <lucasfbassetto@gmail.com>
2026-03-12 16:33:06 -0300

    feat: add exercicio SistemaEscolar

---
+++ src/main/java/org/example/Poo/herancaPolimorfismoInterface/SistemaEscolar/Docente.java
@@ -0,0 +1,15 @@
+package org.example.Poo.herancaPolimorfismoInterface.SistemaEscolar;
+
+public class Docente extends Pessoa {
+
+    private String disciplina;
+
+    public Docente(String nome, int idade, String disciplina) {
+        super(nome, idade);
+        this.disciplina = disciplina;
+    }
+
+    public void exibirDados() {
+        System.out.println("Docente: " + getNome() + ", Idade: " + getIdade() + ", Disciplina: " + disciplina);
+    }
+}
